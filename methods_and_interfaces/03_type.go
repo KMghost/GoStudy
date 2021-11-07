0f4dfa41428490e7a054da9893188a4aeb3afbe0
Daniel <wuyh512@foxmail.com>
2021-11-07 22:04:00 +0800

    stringer 练习

--- methods_and_interfaces/03_type.go
+++ methods_and_interfaces/03_type.go
@@ -71,17 +71,36 @@ import "fmt"
 
 // ========================================================================
 // Stringer
-type Person struct {
-	Name string
-	Age  int
-}
+// type Person struct {
+// 	Name string
+// 	Age  int
+// }
+//
+// func (p Person) String() string {
+// 	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+// }
+//
+// func main() {
+// 	a := Person{"Arthur Dent", 42}
+// 	z := Person{"Zaphod Beeblebrox", 9001}
+// 	fmt.Println(a, z)
+// }
 
-func (p Person) String() string {
-	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
-}
+// ========================================================================
+// Stringer 练习
 
+type IPAddr [4]byte
+
+// TODO: 给 IPAddr 添加一个 "String() string" 方法
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+}
 func main() {
-	a := Person{"Arthur Dent", 42}
-	z := Person{"Zaphod Beeblebrox", 9001}
-	fmt.Println(a, z)
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
 }
